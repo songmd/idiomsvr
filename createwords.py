e8d54a38eb9f5ba53da2b787bc7d56e68632eb43
hero101 <songmaodong@outlook.com>
2018-03-30 22:07:32 +0800

    完善字库

--- createwords.py
+++ createwords.py
@@ -1,18 +1,25 @@
 import sqlite3
-
+import random
 
 def sqlite3_to_words():
     sqlite3_conn = sqlite3.connect('idiom.db')
     sqlite3_cursor = sqlite3_conn.cursor()
     sqlite3_cursor.execute('SELECT chengyu FROM idiom')
 
-    words = set()
+    words = dict()
     for chengyu, in sqlite3_cursor:
+        if len(chengyu) != 4:
+            continue
         for w in chengyu:
-            words.add(w)
-
+            if w in words:
+                words[w] += 1
+            else:
+                words[w] = 1
+    ws = [key for key in words.keys() if words[key] > 3 ]
+    print(len(ws))
+    random.shuffle(ws)
     with open('words.js','w') as f:
-        f.write(str(list(words)))
+        f.write(str(ws))
     # print(list(words))
 
 sqlite3_to_words()
